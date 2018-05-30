9f628fda7920925a37731d127a4a66ca7a42cfa9
kotaprabhakar <kotaprabhakar@gmail.com>
2018-05-30 03:52:51 -0400

    p750 final

--- UVa/p750.cpp
+++ UVa/p750.cpp
@@ -1,14 +1,15 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 const int board_size = 8;
-int cnt = 0;
 
-void putQueen(char (*board)[board_size], int i, int j) { board[i][j] = 1; }
+void putQueen(int (*board)[board_size], int i, int j) { board[i][j] = 1; }
 
-void removeQueen(char (*board)[board_size], int i, int j) { board[i][j] = 0; }
+void removeQueen(int (*board)[board_size], int i, int j) { board[i][j] = 0; }
 
-bool isValidCoord(char (*board)[board_size], int i, int j) {
+bool isValidCoord(int (*board)[board_size], int i, int j) {
   int k = 0, l = 0;
   for (k = 0; k < board_size; k++)
     if (board[k][j])
@@ -31,28 +32,77 @@ bool isValidCoord(char (*board)[board_size], int i, int j) {
   return true;
 }
 
-void backtrack(char (*board)[board_size]) {
-  int i = 0, c = 0;
-  for (i = 0; i < board_size * board_size; i++) {
-    if (isValidCoord(board, i / board_size, i % board_size)) {
-      c++;
-      putQueen(board, i / board_size, i % board_size);
-      backtrack(board);
-      removeQueen(board, i / board_size, i % board_size);
+int getEmptyRow(int (*board)[board_size]) {
+  int i = 0, j = 0, c = 0;
+  for (; i < board_size; i++) {
+    for (j = 0, c = 0; j < board_size; j++)
+      if (board[i][j] == 0)
+        c++;
+    if (c == board_size)
+      return i;
+  }
+  return -1;
+}
+
+void backtrack(int (*board)[board_size], vector<string> &res) {
+  int i = 0, j = 0, c = 0;
+  i = getEmptyRow(board);
+  if (i == -1) {
+    int row, col;
+    char arr[board_size + 1];
+    for (row = 0; row < board_size; row++) {
+      for (col = 0; col < board_size; col++) {
+        if (board[row][col] == 1) {
+          arr[col] = 48 + row + 1;
+          break;
+        }
+      }
+    }
+    arr[board_size] = '\0';
+    res.push_back((string)arr);
+    return;
+  }
+  for (j = 0; j < board_size; j++) {
+    if (isValidCoord(board, i, j)) {
+      putQueen(board, i, j);
+      backtrack(board, res);
+      removeQueen(board, i, j);
     }
   }
-  if (c == 0)
-    cnt++;
 }
 
 int main() {
-  char board[board_size][board_size] = {
-      {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0},
-      {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0},
-      {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0},
-      {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}};
-  board[0][0] = 1;
-  backtrack(board);
-  printf("%d\n", cnt);
+  int board[board_size][board_size] = {{0, 0, 0, 0, 0, 0, 0, 0},
+                                       {0, 0, 0, 0, 0, 0, 0, 0},
+                                       {0, 0, 0, 0, 0, 0, 0, 0},
+                                       {0, 0, 0, 0, 0, 0, 0, 0},
+                                       {0, 0, 0, 0, 0, 0, 0, 0},
+                                       {0, 0, 0, 0, 0, 0, 0, 0},
+                                       {0, 0, 0, 0, 0, 0, 0, 0},
+                                       {0, 0, 0, 0, 0, 0, 0, 0}},
+      N, x, y, i;
+  scanf("%d", &N);
+  while (N--) {
+    i = 1;
+    vector<string> res;
+    scanf("%d %d", &x, &y);
+    board[x - 1][y - 1] = 1;
+    backtrack(board, res);
+    sort(res.begin(), res.end());
+    printf("SOLN       COLUMN\n");
+    printf(" #      1 2 3 4 5 6 7 8\n");
+    printf("\n");
+    for (auto iter : res) {
+      if (i / 10 == 0)
+        printf(" %d      %c %c %c %c %c %c %c %c\n", i++, iter[0], iter[1],
+               iter[2], iter[3], iter[4], iter[5], iter[6], iter[7]);
+      else
+        printf("%d      %c %c %c %c %c %c %c %c\n", i++, iter[0], iter[1],
+               iter[2], iter[3], iter[4], iter[5], iter[6], iter[7]);
+    }
+    board[x - 1][y - 1] = 0;
+    if (N != 0)
+      printf("\n");
+  }
   return 0;
 }
